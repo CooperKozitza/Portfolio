97344ab17e2b9931293d1abc49cfebf1372b92db
CooperKozitza <cooperkozitza@gmail.com>
2024-08-11 19:00:05 -0700

    Added Experience and Skills

--- app/page.tsx
+++ app/page.tsx
@@ -20,23 +20,23 @@ const Home = () => {
   const { activeSection, createSectionRef } = useSectionObserver();
 
   const scrollToSection = useCallback((sectionId: string) => {
-      const section = document.getElementById(sectionId);
-      if (section) {
-          section.scrollIntoView({
-              behavior: 'smooth',
-              block: 'start',
-          });
-      }
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    }
   }, []);
 
   return (
-    <div style={{scrollSnapType: 'y mandatory'}}>
+    <div style={{ scrollSnapType: 'y mandatory' }}>
       <figure className="fixed top-0 left-0 w-full h-dvh" style={{ zIndex: -1 }}>
         <SphereBackground viewName={activeSection} />
       </figure>
-      <section 
-        className={`w-full h-dvh ${styles.scrollSnapTarget}`} 
-        ref={createSectionRef('hero')} 
+      <section
+        className={`w-full h-dvh ${styles.scrollSnapTarget}`}
+        ref={createSectionRef('hero')}
         id="hero"
       >
         <Hero />
@@ -48,7 +48,7 @@ const Home = () => {
         <p className="pb-10 text-center">
           Hello! I&apos;m Cooper, a computational math student and software engineer who thrives on solving complex problems and applying mathematical principles to develop innovative software solutions.
         </p>
-        <section className={styles.teaserGrid} onClick={() => scrollToSection('education')}>
+        <section className={styles.teaserGrid}>
           <Card className={styles.teaserCard + ' group'}>
             <div>
               <header className={styles.subsectionHeader}>Education</header>
@@ -56,7 +56,7 @@ const Home = () => {
                 Exploring the depths of mathematics and its applications in modern computing.
               </p>
             </div>
-            <a className={styles.teaserLink}>
+            <a className={styles.teaserLink} onClick={() => scrollToSection('education')}>
               <span className="hidden md:block">
                 Learn More
               </span>
@@ -72,8 +72,7 @@ const Home = () => {
                 Three years of engineering software and effective algorithms in a professional setting.
               </p>
             </div>
-            {/*
-            <a className={styles.teaserLink} href="">
+            <a className={styles.teaserLink} onClick={() => scrollToSection('experience')}>
               <span>
                 Learn More
               </span>
@@ -81,7 +80,6 @@ const Home = () => {
                 <FaChevronRight />
               </figure>
             </a>
-            */}
           </Card>
           <Card className={styles.teaserCard + ' group'}>
             <div>
@@ -103,10 +101,10 @@ const Home = () => {
           </Card>
         </section>
       </Section>
-      <Section 
-        className={styles.scrollSnapTarget} 
-        ref={createSectionRef('education')} 
-        id="education" 
+      <Section
+        className={styles.scrollSnapTarget}
+        ref={createSectionRef('education')}
+        id="education"
         right
       >
         <header className={styles.sectionHeader}>
@@ -160,7 +158,75 @@ const Home = () => {
           </section>
         </Card>
       </Section>
-    </div>
+      <Section
+        className={styles.scrollSnapTarget}
+        ref={createSectionRef('experience')}
+        id="experience"
+        left
+      >
+        <header className={styles.sectionHeader}>
+          Experience
+        </header>
+        <Card className="max-w-2xl">
+          <section className="pb-8">
+            <time className={styles.monoText}>November 2021 - Present</time>
+            <header className={styles.subsectionHeader + ' flex items-center}'}>
+              Intern, Cascade Custom Software LLC, Portland
+            </header>
+            <div className="space-y-4">
+              <p>
+                Individually spearheaded the development of comprehensive inventory and company management software tools, deployed across desktop, web, and mobile platforms, for major companies like Tidewater Barge Lines, Tidewater Enviromental Services, Daimler Truck North America, and Domtar.
+              </p>
+              <p>
+                In this role, I maintained and enhanced existing codebases, implementing new features and resolving bugs for software relied upon by more than 3,000 users.
+                Additionally, I contributed to the modernization of a massive codebase consisting of millions of lines of code, ensuring the software remained robust and scalable.
+              </p>
+            </div>
+          </section>
+          <section className="pb-8">
+            <time className={styles.monoText}>February 2024</time>
+            <header className={styles.subsectionHeader + ' flex items-center}'}>
+              Research Assistant, Casey Eye Institute at OHSU
+            </header>
+            <p>
+              Collaborated closely with Dr. Alireza Karimi on pioneering research that developed an AI-driven classification model designed to assist in the early diagnosis of glaucoma based on demographic data.
+              My contributions to this project culminated in a co-authorship as the 3rd author on a published paper.
+            </p>
+          </section>
+        </Card>
+      </Section >
+      <Section
+        className={styles.scrollSnapTarget}
+        ref={createSectionRef('skills')}
+        id="skills"
+      >
+        <header className={styles.sectionHeader}>
+          Skills
+        </header>
+        <Card className="max-w-2xl">
+          <section className="pb-8">
+            <header className={styles.subsectionHeader}>
+              Programming Languages
+            </header>
+            <ul className="list-disc pl-5 space-y-4">
+              <li><strong>C/C++ (6 Years)</strong>: Expertise in optimizing graphics rendering using OpenGL and Vulkan. Strong understanding of the standard library and advanced language features.</li>
+              <li><strong>C# (3 Years)</strong>: Experienced in developing server-side web applications and APIs using the .NET framework. Proficient in game development with Unity.</li>
+              <li><strong>JavaScript/TypeScript (3 Years)</strong>: Skilled in building interactive applications using React. Experienced in creating web servers and APIs with Node.js.</li>
+            </ul>
+          </section>
+          <section className="pb-8">
+            <header className={styles.subsectionHeader}>
+              Tools & Technologies
+            </header>
+            <ul className="list-disc pl-5 space-y-4">
+              <li><strong>Development Tools</strong>: Proficient in Vi, VS Code, Visual Studio, and Git for version control. Experienced in project management using GitHub and Azure DevOps.</li>
+              <li><strong>Databases</strong>: Capable of creating, editing, and managing SQL-based databases using object-relational mapping (ORM) or raw SQL queries.</li>
+              <li><strong>Cloud Platforms</strong>: Experience in building and deploying applications hosted on cloud platforms like Google Cloud Platform (GCP) and Microsoft Azure.</li>
+            </ul>
+          </section>
+        </Card>
+      </Section>
+    </div >
   )
 }
 
